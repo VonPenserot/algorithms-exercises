ea67de5893f6f45476591549ecce9fef1b30de8e
VonPenserot <joachim.penserot@proton.me>
2025-05-08 07:56:17 -0300

    implement recursion for factorial problem

--- specs/recursion/factorials.test.js
+++ specs/recursion/factorials.test.js
@@ -10,11 +10,18 @@
   factorial(3) = 6 
 */
 
-function factorial(n) {}
+function factorial(n) {
+  let result = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (n < 2) return 1;
+    return n * factorial(n - 1);
+  }
+}
 
 // unit tests
 // do not modify the below code
-test.skip("factorials", () => {
+test("factorials", () => {
   expect(factorial(1)).toEqual(1);
   expect(factorial(2)).toEqual(2);
   expect(factorial(3)).toEqual(6);
