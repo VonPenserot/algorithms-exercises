38a131e1e03e9b76c7f60c6a49c8aafa8c722c4d
VonPenserot <joachim.penserot@proton.me>
2025-05-09 08:01:00 -0300

    implement binary search

--- specs/search/search.test.js
+++ specs/search/search.test.js
@@ -13,7 +13,27 @@ function linearSearch(id, array) {
   return void 0;
 }
 
-function binarySearch(id, array) {}
+function binarySearch(id, array) {
+  let min = 0;
+  let max = array.length - 1;
+  let index;
+  let element;
+
+  while (min <= max) {
+    index = Math.floor((min + max) / 2);
+    element = array[index];
+
+    if (element.id < id) {
+      min = index + 1;
+    } else if (element.id > id) {
+      max = index - 1;
+    } else {
+      return element;
+    }
+  }
+
+  return void 0;
+}
 
 // unit tests
 // do not modify the below code
